a3cc6ccf7104e68b272dfec8e0bfabf4ba906ea7
Alejandro Gómez <algono@inf.upv.es>
2019-06-23 01:13:08 +0200

    Updated to Java 11

---
+++ src/accesoupv/Main.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package accesoupv;
+
+import accesoupv.controller.PrincipalController;
+import accesoupv.model.AccesoUPV;
+import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
+import javafx.stage.WindowEvent;
+
+/**
+ *
+ * @author Alejandro
+ */
+public class Main extends Application {
+    
+    @Override
+    public void start(Stage stage) throws Exception {
+        
+        FXMLLoader myLoader = new FXMLLoader(getClass().getResource("/accesoupv/view/PrincipalView.fxml"));
+        Parent root = (Parent) myLoader.load();
+        stage.setResizable(false);
+        Scene scene = new Scene(root);
+        stage.setTitle("Acceso UPV");
+        stage.getIcons().add(new Image(getClass().getResourceAsStream("/accesoupv/resources/icons/app-icon.png")));
+        stage.setScene(scene);
+        stage.setOnCloseRequest((WindowEvent we) -> {
+            if (AccesoUPV.getInstance().shutdown()) Platform.exit(); 
+            else we.consume();
+        });
+        myLoader.<PrincipalController>getController().initStage(stage);
+        stage.show();
+    }
+}
