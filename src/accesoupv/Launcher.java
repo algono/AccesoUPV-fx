ae15f44d058cb2394655a13dab61c16d4c29e621
Alejandro Gómez <algono@inf.upv.es>
2019-07-07 18:04:38 +0200

    Revert "Updated to Java 11" Java 11 is not prepared for JavaFX deployment. This reverts commit a3cc6ccf7104e68b272dfec8e0bfabf4ba906ea7.

--- src/accesoupv/Launcher.java
+++ src/accesoupv/Launcher.java
@@ -5,18 +5,46 @@
  */
 package accesoupv;
 
+import accesoupv.controller.PrincipalController;
+import accesoupv.model.AccesoUPV;
 import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
+import javafx.stage.WindowEvent;
 
 /**
  *
  * @author Alejandro
  */
-public class Launcher {
+public class Launcher extends Application {
+    
+    @Override
+    public void start(Stage stage) throws Exception {
+        
+        FXMLLoader myLoader = new FXMLLoader(getClass().getResource("/accesoupv/view/PrincipalView.fxml"));
+        Parent root = (Parent) myLoader.load();
+        stage.setResizable(false);
+        Scene scene = new Scene(root);
+        stage.setTitle("Acceso UPV");
+        stage.getIcons().add(new Image(getClass().getResourceAsStream("/accesoupv/resources/icons/app-icon.png")));
+        stage.setScene(scene);
+        stage.setOnCloseRequest((WindowEvent we) -> {
+            if (AccesoUPV.getInstance().shutdown()) Platform.exit(); 
+            else we.consume();
+        });
+        myLoader.<PrincipalController>getController().initStage(stage);
+        stage.show();
+    }
+
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        Application.launch(Main.class, args);
+        launch(args);
     }
     
 }
